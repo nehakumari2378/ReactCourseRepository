b9f3f37b928330d7d4ae25cd2de59c9887e98623
Neha Kumari <neha.kumari1@hp.com>
2021-02-06 11:07:51 +0530

    first Component commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi I am react app</h1>
+        <Person />
+      </div>
+    );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now'));
+  }
+}
+
+export default App;
