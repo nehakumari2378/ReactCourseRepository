4eb6dccd1a053baf291b4fe07f15a094bce394ba
Neha Kumari <nehakumari2378@gmail.com>
2021-02-12 12:37:42 +0530

    Passing method references and two way binding

--- src/App.js
+++ src/App.js
@@ -1,87 +1,104 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
 
-//*******************Function based component*********************
-const app = props => {
+//*****************Class based component******************
+class App extends Component {
 
-  const [personsState, setPersonsState]= useState({
+  state= {
     person: [
       {name: 'Rai', age: 28},
       {name:'Neha', age:27}
     ]
-  });
-  
-  const [otherState, setOtherState] = useState('Some other state');
+  }
 
-  console.log(personsState, otherState);
-
-  const switchNameHandler = () => {
-    setPersonsState({
+  switchNameHandler = (newName) => {
+    //console.log('Was clicked!!')
+    //DOn't do this:  this.state.person[0].name = 'Rai2';
+    this.setState({
       person: [
-        {name: 'Rai2', age: 28},
+        {name: newName, age: 28},
         {name:'Neha', age:27}
       ]
-    });
-  };
+    })
+  }
 
+  onChangedHandler = (event) => {
+    this.setState({
+      person: [
+        {name: event.target.value, age: 28},
+        {name:'Neha', age:27}
+      ]
+    })
+  }
+
+  render() {
     return (
       <div className="App">
         <h1>Hi I am react app</h1>
         {/* <Person name="Neha" age="27"/>
         <Person name="Rai" age="28">Hobbies: Reading</Person> */}
-
-        <button onClick={switchNameHandler}>Switch name</button>
-        <Person name={personsState.person[0].name} age={personsState.person[0].age}/>
-        <Person name={personsState.person[1].name} age={personsState.person[1].age}>Hobbies: Reading</Person>
+        
+        <button onClick={this.switchNameHandler.bind(this, 'Rai2')}>Switch name</button>
+        <Person 
+          name={this.state.person[0].name} 
+          age={this.state.person[0].age}
+          changed={this.onChangedHandler}/>
+        <Person 
+          name={this.state.person[1].name} 
+          age={this.state.person[1].age} 
+          click={this.switchNameHandler.bind(this, 'Rai2!!')}>Hobbies: Reading</Person>
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now'));
-  
+  }
 }
 
-export default app;
+export default App;
 
 
 
+//*******************Function based component*********************
+// const app = props => {
 
-
-//*****************Class based component******************
-// class App extends Component {
-
-//   state= {
+//   const [personsState, setPersonsState]= useState({
 //     person: [
 //       {name: 'Rai', age: 28},
 //       {name:'Neha', age:27}
 //     ]
-//   }
+//   });
+  
+//   const [otherState, setOtherState] = useState('Some other state');
 
-//   switchNameHandler = () => {
-//     //console.log('Was clicked!!')
-//     //DOn't do this:  this.state.person[0].name = 'Rai2';
-//     this.setState({
+//   console.log(personsState, otherState);
+
+//   const switchNameHandler = () => {
+//     setPersonsState({
 //       person: [
 //         {name: 'Rai2', age: 28},
 //         {name:'Neha', age:27}
 //       ]
-//     })
-//   }
+//     });
+//   };
 
-//   render() {
 //     return (
 //       <div className="App">
 //         <h1>Hi I am react app</h1>
 //         {/* <Person name="Neha" age="27"/>
 //         <Person name="Rai" age="28">Hobbies: Reading</Person> */}
 
-//         <button onClick={this.switchNameHandler}>Switch name</button>
-//         <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
-//         <Person name={this.state.person[1].name} age={this.state.person[1].age}>Hobbies: Reading</Person>
+//         <button onClick={switchNameHandler}>Switch name</button>
+//         <Person name={personsState.person[0].name} age={personsState.person[0].age}/>
+//         <Person name={personsState.person[1].name} age={personsState.person[1].age}>Hobbies: Reading</Person>
 //       </div>
 //     );
 //     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now'));
-//   }
+  
 // }
 
-// export default App;
+// export default app;
+
+
+
+
